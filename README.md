3af59fdc52425c9aa206e2d880aa1290885bf33c
Shubham Mazumder <shubham.mazumder@gmail.com>
2024-09-15 16:08:27 -0600

    commit final

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# wordle
-Wordle, built with Next.js and Tailwind
+<br/>
+
+  <h1 align="center">Wordle Clone</h1>
+
+  <p align="center">
+    Wordle Clone built with Next.js and tailwindcss.
+    <br />
+  </p>
+</p>
+
+<br />
+
+## Development
+
+Steps to run Wordle Clone in your local environment.
+
+1. Clone the repository
+
+   ```sh
+   git clone https://github.com/cystema/wordle
+   ```
+
+2. Go to the project directory
+
+   ```sh
+   cd wordle
+   ```
+
+3. Install the NPM packages
+
+   ```sh
+   npm install
+   ```
+
+4. Start the development server
+
+   ```sh
+   npm run dev
+   ```
+
+5. Visit http://localhost:3000
+
+<br/>
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import {IconGithub, IconHelp} from '@icons';
+import {ExternalLink, ThemeButton, HelpModal} from '@components';
+import {useModal} from '@hooks';
+
+export function Header() {
+  const {setModalContent} = useModal();
+  return (
+    <header className="flex h-header w-full items-center justify-center border-b border-gray-30 px-3 text-4xl dark:border-black-10">
+      <ExternalLink href="https://github.com/cystema/wordle">
+        <IconGithub className="h-7 w-7 opacity-100 hover:opacity-60" />
+      </ExternalLink>
+      <IconHelp
+        className="ml-2 h-7 w-7 cursor-pointer opacity-100 hover:opacity-80"
+        onClick={() =>
+          setModalContent({
+            renderContent: () => <HelpModal />,
+            title: 'HOW TO PLAY',
+          })
+        }
+      />
+      <div className="flex-1 text-center text-xl font-bold xsm:text-4xl">
+        Wordle
+      </div>
+      <ThemeButton className="ml-9" />
+    </header>
+  );
+}
